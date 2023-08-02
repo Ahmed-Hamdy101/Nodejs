f26eef760d8d6bff2cf199e7d5cb3a27797866bc
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-08-03 00:36:50 +0200

    Chapter 3 🥈

---
+++ ch3_run_js2/index.js
@@ -0,0 +1,5 @@
+// this ES6
+import test from "./util/util.js";
+console.log(
+    test("mission executed...")
+);
---
+++ ch3_run_js2/util/util.js
@@ -0,0 +1,12 @@
+//  anonymous function
+const test = (t) => {
+  if (isNaN(t) ) {
+    return ". - -   . . .  - .  - ";
+    console.log("debugging node ... ");
+  }else{
+    return false;
+    console.log("Node v17 !");
+  }
+};
+
+export default test;
