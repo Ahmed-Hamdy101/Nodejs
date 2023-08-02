24f55f415b68b13034646ba29e69cbb074e5b66e
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-08-03 01:14:45 +0200

    Chapter 2 🥈

--- ch2_run_js1/index.js
+++ ch2_run_js1/index.js
@@ -1,3 +1,3 @@
-const {file} = require('./src/util/util');// #method1
+const {file} = require('./util/util');// #method1
 // require("./src/util/util"); //method2
 console.log(file);
--- ch2_run_js1/util/util.js
+++ ch2_run_js1/util/util.js
@@ -1,3 +1,8 @@
-
-const file = __filename.split('').join('/');
-module.exports={file};
+/* 
+spilt : seprate the file if there any space
+join : replace with '/'
+*/
+const file = __filename.split(' ').join('/');
+module.exports = {
+  file,
+};
