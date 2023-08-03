cb6cd347ed4fdc2ceef00f8981cddae768182b2d
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-08-03 03:26:16 +0300

    Chapter 3 node run 🐱‍🚀

--- ch3_run_js2/util/util.js
+++ ch3_run_js2/util/util.js
@@ -1,12 +1,15 @@
 //  anonymous function
 const test = (t) => {
+  // if is not number
   if (isNaN(t) ) {
     return ". - -   . . .  - .  - ";
     console.log("debugging node ... ");
-  }else{
+  }
+  // else try another thing
+  else{
     return false;
     console.log("Node v17 !");
   }
 };
-
+// export the function
 export default test;
