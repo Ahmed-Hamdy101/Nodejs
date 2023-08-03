c4c3f345a1be2cb239de92e06a3a19efd6e8769b
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-08-03 03:46:17 +0300

    Chapter 4 util & module system ⚒

--- ch4_module_system/util/util.js
+++ ch4_module_system/util/util.js
@@ -1,6 +1,6 @@
-// Get filename
-const file = __filename.split('/').join('/')
-// Get Dirname
-const dir = __dirname.split('/').join('/')
+// split the path of file by ' ' name and add '/'
+const file = __filename.split(' ').join('/')
+// split the path of dir by ' ' name and add '/'
+const dir = __dirname.split(' ').join('/')
 //export file to call it in another file
 module.exports={file,dir};
