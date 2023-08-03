b6fb658282a63007c5e98d265e9eca1d64147b51
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-08-03 02:59:30 +0200

    Chapter 7 utility :beer:

--- ch7_utility_lib/index.js
+++ ch7_utility_lib/index.js
@@ -1,6 +1,10 @@
-const util = require('../ch7_utility_lib/util/u-2')
-const {items , lg } = require('./util/u-1');
-
-console.log(util.sum([1,2,4]),util.links([5,2,4] , [1,3,3]));
-console.log(items([1,2,3]));
-console.log(lg([1,2,3]));
+// import file from utol
+const util = require("../ch7_utility_lib/util/u-2");
+// import from items and large from u-1 
+const { items, lg } = require("./util/u-1");
+// log the function links : add both array function in one array
+console.log(util.sum([1, 2, 4]), util.links([5, 2, 4], [1, 3, 3]));
+// remove one item 
+console.log(items([1, 2, 3]));
+//get large rumber
+console.log(lg([1, 2, 3]));
